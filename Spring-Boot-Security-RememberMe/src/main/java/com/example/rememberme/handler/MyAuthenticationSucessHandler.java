730d3119565cd15c01875c6e7a023fbca449e9dd
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-02-23 15:47:36 +0800

    新增SpringBoot集成SpringSecurity实现RememberMe功能示例

---
+++ Spring-Boot-Security-RememberMe/src/main/java/com/example/rememberme/handler/MyAuthenticationSucessHandler.java
@@ -0,0 +1,43 @@
+package com.example.rememberme.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author dengzhiming
+ * @date 2020/2/19 20:59
+ */
+@Component
+public class MyAuthenticationSucessHandler implements AuthenticationSuccessHandler {
+    /*@Autowired
+    private ObjectMapper mapper;
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                        Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.setContentType("application/json;charset=utf-8");
+        httpServletResponse.getWriter().write(mapper.writeValueAsString(authentication));
+    }*/
+    /*@Autowired
+    private ObjectMapper mapper;
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, httpServletResponse);
+        redirectStrategy.sendRedirect(httpServletRequest, httpServletResponse, savedRequest.getRedirectUrl());
+    }*/
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        redirectStrategy.sendRedirect(request, response, "/index");
+    }
+}
