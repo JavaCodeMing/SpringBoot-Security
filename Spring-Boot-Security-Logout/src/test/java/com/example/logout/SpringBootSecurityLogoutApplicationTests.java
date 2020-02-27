634e20ee31941802cc19c3b2d6ba7c50b87bdefb
dengzhiming <36990141+JavaCodeMing@users.noreply.github.com>
2020-02-27 23:18:05 +0800

    新增SpringBoot集成SpringSecurity实现退出登录逻辑

---
+++ Spring-Boot-Security-Logout/src/test/java/com/example/logout/SpringBootSecurityLogoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityLogoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
